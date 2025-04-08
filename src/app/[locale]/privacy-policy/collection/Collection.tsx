d45e6d023571ec96dc3b410c2580d82d1a05a5d8
LevanidzeGe <levanidzelevani@gmail.com>
2025-04-08 11:27:09 +0200

    1

--- src/app/[locale]/privacy-policy/collection/Collection.tsx
+++ src/app/[locale]/privacy-policy/collection/Collection.tsx
@@ -2,7 +2,7 @@ import { fetchCollectionIfUpdated } from "@/src/lib/firebase/getFirebaseData";
 import styles from "./Collection.module.css";
 import { extractCollectionFields } from "@/src/lib/firebase/types";
 import { getLocale } from "next-intl/server";
-import { companyName, address, email, phoneNumber } from "@/src/Manager/info";
+import { companyName, address, email, phoneNumber } from "@/src/manager/info";
 
 export function replacePlaceholders(text: string): string {
   return text
